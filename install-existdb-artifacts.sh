6119cf61738ec570ecc70ea7cd7a0f3ed5f62f13
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2017-06-28 10:01:52 +0000

    add -e so ansible can run it automatically...

--- install-existdb-artifacts.sh
+++ install-existdb-artifacts.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+ECHOMODE=echo
+EXECMODE=$ECHOMODE
+# Parse command-line parameters
+while getopts "e" option
+    # : after option shows it will have an argument passed with it.
+do
+    case $option in
+        e )
+            EXECMODE="exec";;
+        * ) # Unknown option
+            echo "Invalid option -- $option"
+            exit 0 ;;
+    esac
+done
+
+
+
+
 # try to find the existdb location from pre defined values and use first one
 searchPaths=$(echo $HOME/e{X,x}ist{-db,db,})
 existdbpaths=( $(ls -ad  $HOME/e{X,x}ist{-db,db,} $USER_EXISTDB_ROOT 2> /dev/null) )
@@ -27,7 +45,12 @@ for jar in $jars
 do
   cmd="mvn install:install-file -Dfile=$basedir/$jar.jar -DgroupId=org.exist-db -DartifactId=exist-$(basename $jar) -Dversion=$version -Dpackaging=jar"
   echo "# installing artifactId=$jar"
-  echo "$cmd" 
+  if [ $EXECMODE = $ECHOMODE ]
+  then
+    echo "$cmd"
+  else 
+    $cmd 
+  fi
 done
 
 echo "# Please cut and paste this section or check consistency with your pom file"
